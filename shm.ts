1710d15e7c472bae104c1ec465ba53f1545fa6a2
Tamotsu Takahashi <ttakah+github@gmail.com>
2024-04-13 23:22:33 +0900

    uncomment the previous commit

--- shm.ts
+++ shm.ts
@@ -14,7 +14,7 @@ import { Feed } from 'https://jspm.dev/feed';
 // refresh が true ならそれらの前に kv を全部消してから始める
 
 const myname = 'shm';
-// const selflink = 'https://shm-rss.deno.dev/'; // validator を黙らせたければ
+const selflink = ''; // 'https://shm-rss.deno.dev/';
 const refresh = false;
 const ttl = 60;
 const storedays = 31;
@@ -163,7 +163,7 @@ async function handler(_req: Request): Promise<Response> {
             description: await kvval('description'),
             updated: new Date(parseInt(await kvval('updated'))),
             ttl: ttl,
-            // feed: selflink,
+            feed: selflink,
         });
 
         type FeedItem = Omit<Item, 'date'> & {
