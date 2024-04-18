13c58ba4578a0dd62f962f2a4ff13de698cf589b
Tamotsu Takahashi <ttakah+github@gmail.com>
2024-04-18 12:35:32 +0900

    Fix tests for vscode

--- shm.test.ts
+++ shm.test.ts
@@ -1,69 +1,72 @@
 import { SHM } from "./shm.ts";
 import { assertEquals } from "jsr:@std/assert"; // "https://deno.land/std/assert/mod.ts";
+
 // import FakeTimers from "npm:@sinonjs/fake-timers"; // 現在時刻に左右されるとき使う
+// const faketimer = FakeTimers.install();
+// faketimer.setSystemTime(new Date("2024-04-14"));
 
-const snapshots = [
-  {
-    date: "20240414",
-    log: [
-      "no title",
-      '<a class="NU" href="https://www.st.ryukoku.ac.jp/~kjm/security/memo/2024/04.html#20240412_">■</a>',
-    ],
-  },
-  {
-    date: "20240417",
-    log: [],
-  },
-];
-
-snapshots.forEach((s) =>
-  Deno.test(
-    `${s.date} snapshot (needs --unstable-kv)`,
-    async (t) => {
-      // const faketimer = FakeTimers.install();
-      // faketimer.setSystemTime(new Date("2024-04-14"));
-
-      const denokv = await Deno.openKv(`./testdata/${s.date}.kv`);
-      const shm = new SHM();
-
-      await t.step("html2kv", async () => {
-        const logs: string[] = [];
-        const origlog = globalThis.console.log;
-        globalThis.console.log = (a1: string, a2 = "") => {
-          logs.push(a1, a2);
-        };
-
-        const html = Deno.readTextFileSync(`./testdata/${s.date}.html`);
-        await shm.html2kv(html, denokv);
-
-        globalThis.console.log = origlog;
-        assertEquals(logs, s.log);
-      });
-
-      await t.step("kv2feed (rss)", async () => {
-        const rss = (await shm.kv2feed(denokv)).rss2();
-        Deno.writeTextFileSync(`./testdata/${s.date}.rss`, rss); // デバッグ用
-        const expectedrss = Deno.readTextFileSync(
-          `./testdata/${s.date}.expected.rss`,
-        );
-        assertEquals(rss, expectedrss);
-        Deno.removeSync(`./testdata/${s.date}.rss`); // 失敗時には残る
-      });
-
-      await t.step("kv2feed (atom)", async () => {
-        shm.selflink = "https://shm-rss.deno.dev/";
-
-        const atom = (await shm.kv2feed(denokv)).rss2();
-        Deno.writeTextFileSync(`./testdata/${s.date}.atom`, atom); // デバッグ用
-        const expectedatom = Deno.readTextFileSync(
-          `./testdata/${s.date}.expected.atom`,
-        );
-        assertEquals(atom, expectedatom);
-        Deno.removeSync(`./testdata/${s.date}.atom`); // 失敗時には残る
-      });
-
-      denokv.close();
-      Deno.removeSync(`./testdata/${s.date}.kv`);
-    },
-  )
+Deno.test(
+  "20240414 snapshot (needs --unstable-kv)",
+  async (t) =>
+    await snaptester({
+      date: "20240414",
+      log: [
+        "no title",
+        '<a class="NU" href="https://www.st.ryukoku.ac.jp/~kjm/security/memo/2024/04.html#20240412_">■</a>',
+      ],
+    }, t),
 );
+
+Deno.test(
+  "20240417 snapshot (needs --unstable-kv)",
+  async (t) =>
+    await snaptester({
+      date: "20240417",
+      log: [],
+    }, t),
+);
+
+type Snapshot = { date: string; log: string[] };
+async function snaptester(s: Snapshot, t: Deno.TestContext) {
+  const denokv = await Deno.openKv(`./testdata/${s.date}.kv`);
+  const shm = new SHM();
+
+  await t.step("html2kv", async () => {
+    const logs: string[] = [];
+    const origlog = globalThis.console.log;
+    globalThis.console.log = (a1: string, a2 = "") => {
+      logs.push(a1, a2);
+    };
+
+    const html = Deno.readTextFileSync(`./testdata/${s.date}.html`);
+    await shm.html2kv(html, denokv);
+
+    globalThis.console.log = origlog;
+    assertEquals(logs, s.log);
+  });
+
+  await t.step("kv2feed (rss)", async () => {
+    const rss = (await shm.kv2feed(denokv)).rss2();
+    Deno.writeTextFileSync(`./testdata/${s.date}.rss`, rss); // デバッグ用
+    const expectedrss = Deno.readTextFileSync(
+      `./testdata/${s.date}.expected.rss`,
+    );
+    assertEquals(rss, expectedrss);
+    Deno.removeSync(`./testdata/${s.date}.rss`); // 失敗時には残る
+  });
+
+  await t.step("kv2feed (atom)", async () => {
+    shm.selflink = "https://shm-rss.deno.dev/";
+
+    const atom = (await shm.kv2feed(denokv)).rss2();
+    Deno.writeTextFileSync(`./testdata/${s.date}.atom`, atom); // デバッグ用
+    const expectedatom = Deno.readTextFileSync(
+      `./testdata/${s.date}.expected.atom`,
+    );
+    assertEquals(atom, expectedatom);
+    Deno.removeSync(`./testdata/${s.date}.atom`); // 失敗時には残る
+  });
+
+  denokv.close();
+  Deno.removeSync(`./testdata/${s.date}.kv`);
+}
